89439fbbf2f3d872891ced649eb5468730df3406
thirdpartofkalista <dreams.fulfilled@yahoo.com>
2018-10-09 21:28:22 +0300

    robot class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Robot {
+
+    //    movement motors
+    public DcMotor right_front = null;
+    public DcMotor right_back = null;
+    public DcMotor left_front = null;
+    public DcMotor left_back = null;
+
+    public void init(HardwareMap hw) {
+        // TODO: 10/9/2018 add hardware
+        right_front = hw.get(DcMotor.class, "right_front");
+        right_back = hw.get(DcMotor.class, "right_back");
+        left_front = hw.get(DcMotor.class, "left_front");
+        left_back = hw.get(DcMotor.class, "left_back");
+    }
+
+    public void initDrivetrain() {
+        left_front.setDirection(DcMotor.Direction.FORWARD);
+        right_front.setDirection(DcMotor.Direction.FORWARD);
+        left_back.setDirection(DcMotor.Direction.FORWARD);
+        right_back.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+
+}
+
+
+
+
+
+
+
