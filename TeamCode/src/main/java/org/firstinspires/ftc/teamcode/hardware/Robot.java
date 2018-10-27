c78ce00c85f6c1882b953c00afae65e35daa33c2
vectron <teamvectron@gmail.com>
2018-10-27 12:00:05 -0700

    Test java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Robot.java
@@ -2,9 +2,6 @@ package org.firstinspires.ftc.teamcode.hardware;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-
-import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Robot {
 
