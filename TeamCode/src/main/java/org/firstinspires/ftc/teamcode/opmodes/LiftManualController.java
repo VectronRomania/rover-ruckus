8304412fb3bfc30aedc5aaa3cbd747e2c50177f5
cezarmathe <cezarmathe@gmail.com>
2019-02-26 16:45:36 +0200

    safe

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LiftManualController.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.hardware.Robot;
+import org.firstinspires.ftc.teamcode.systems.opmode.TeleOpStandard;
+
+@TeleOp(name = "Lift manual controller", group = "test")
+public class LiftManualController extends TeleOpStandard {
+    @Override
+    public void initialize() {
+        Robot.Lift.setDirection(DcMotorSimple.Direction.FORWARD, DcMotorSimple.Direction.REVERSE);
+        Robot.Lift.setRunMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Robot.Lift.setRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    @Override
+    public void opModeLoop() {
+        Robot.Lift.setPower(-gamepad1.left_stick_y, -gamepad1.right_stick_y);
+    }
+}
