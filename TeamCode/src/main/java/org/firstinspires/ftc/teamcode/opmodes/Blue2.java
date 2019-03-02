145cfb121838b139f53ae0ef78da03033c9e218f
cezarmathe <cezarmathe@gmail.com>
2019-03-02 08:41:39 +0200

    Parking for autonomous when facing the crater

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Blue2.java
@@ -3,18 +3,21 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.hardware.Robot;
+import org.firstinspires.ftc.teamcode.systems.Extender;
 import org.firstinspires.ftc.teamcode.systems.Lift;
+import org.firstinspires.ftc.teamcode.systems.autonomous.ClaimerParker;
 import org.firstinspires.ftc.teamcode.systems.autonomous.LiftDeploy;
 import org.firstinspires.ftc.teamcode.systems.autonomous.MineralDetector;
 import org.firstinspires.ftc.teamcode.systems.autonomous.MineralSampler;
 import org.firstinspires.ftc.teamcode.systems.drivetrain.AutonomousDrivetrain;
 import org.firstinspires.ftc.teamcode.systems.drivetrain.WheelBase;
 import org.firstinspires.ftc.teamcode.systems.opmode.AutonomousStandard;
-import org.firstinspires.ftc.teamcode.systems.telemetry.items.DrivetrainEncoderTelemetryGroup;
 import org.firstinspires.ftc.teamcode.systems.util.BackgroundTask;
 
-@Autonomous(name = "Deploy+sample", group = "autonomous")
-public class Blue1 extends AutonomousStandard {
+@Autonomous(name = "Deploy+park", group = "autonomous")
+public class Blue2 extends AutonomousStandard {
+
+    private Extender extender;
 
     private MineralDetector mineralDetector;
     private BackgroundTask<String> mineralDetectorTask;
@@ -23,11 +26,15 @@ public class Blue1 extends AutonomousStandard {
 
     private LiftDeploy liftDeploy;
 
+    private ClaimerParker claimerParker;
+
     @Override
     protected void initialize() {
         Robot.Lift.setPower(0.1, -0.1);
         drivetrain = new AutonomousDrivetrain(WheelBase.MECANUM);
         drivetrain.init();
+
+        extender = new Extender();
 
         mineralDetector = new MineralDetector(hardwareMap, this);
         mineralDetectorTask = mineralDetector.getDetector();
@@ -36,6 +43,8 @@ public class Blue1 extends AutonomousStandard {
         liftDeploy = new LiftDeploy(new Lift(false), drivetrain, this);
 
         mineralSampler = new MineralSampler(drivetrain, this);
+
+        claimerParker = new ClaimerParker(drivetrain, this, extender);
 
         telemetry.addData("imu", "calibrating");
         telemetry.update();
@@ -48,11 +57,9 @@ public class Blue1 extends AutonomousStandard {
 
     @Override
     protected void opModeLoop() {
-//        Start the mineral detector
-        mineralDetectorTask.start();
+
         telemetryManager.add(mineralDetectorTask.getStatusTelemetryItem());
         telemetryManager.add(mineralDetectorTask.getRunnableTelemetryItem());
-
 
 //        Start the deployment task and wait for it to finish
         BackgroundTask<Integer> dropDownBackgroundTask = liftDeploy.getDeployTask();
@@ -62,10 +69,6 @@ public class Blue1 extends AutonomousStandard {
         dropDownBackgroundTask.start();
         while(opModeIsActive() && !dropDownBackgroundTask.isFinished()) {
             telemetryManager.cycle();
-//            if fully landed and ready to move, switch to deployed
-            if (dropDownBackgroundTask.getResult() == 4) {
-                mineralDetector.switchToDeployed();
-            }
             idle();
         }
         if (!opModeIsActive()) {
@@ -77,21 +80,33 @@ public class Blue1 extends AutonomousStandard {
 
 //        Switch the detector to deployed detection
         mineralDetector.switchToDeployed();
+        mineralDetectorTask.start();
 
-//        Create the sampling task, start it and wait for it to finish
-        BackgroundTask mineralSamplingBackgroundTask = mineralSampler.sample(mineralDetector);
-        int sampling_index1 = telemetryManager.add(mineralSamplingBackgroundTask.getStatusTelemetryItem());
-        int sampling_index2 = telemetryManager.add(mineralSamplingBackgroundTask.getRunnableTelemetryItem());
+////        Create the sampling task, start it and wait for it to finish
+//        BackgroundTask mineralSamplingBackgroundTask = mineralSampler.sample(mineralDetector);
+//        int sampling_index1 = telemetryManager.add(mineralSamplingBackgroundTask.getStatusTelemetryItem());
+//        int sampling_index2 = telemetryManager.add(mineralSamplingBackgroundTask.getRunnableTelemetryItem());
+//
+//        mineralSamplingBackgroundTask.start();
+//        while (opModeIsActive() && !mineralSamplingBackgroundTask.isFinished()) {
+//            telemetryManager.cycle();
+//            idle();
+//        }
+//        if (!opModeIsActive()) {
+//            return;
+//        }
+//        telemetryManager.remove(sampling_index1);
+//        telemetryManager.remove(sampling_index2);
 
-        mineralSamplingBackgroundTask.start();
-        while (opModeIsActive() && !mineralSamplingBackgroundTask.isFinished()) {
+//        park
+
+        BackgroundTask<Integer> claimingParkingTask = claimerParker.getBackGroundTask(false);
+        claimingParkingTask.start();
+        telemetryManager.add(claimingParkingTask.getStatusTelemetryItem());
+        telemetryManager.add(claimingParkingTask.getRunnableTelemetryItem());
+        while (!claimingParkingTask.isFinished() && opModeIsActive()) {
             telemetryManager.cycle();
             idle();
         }
-        if (!opModeIsActive()) {
-            return;
-        }
-        telemetryManager.remove(sampling_index1);
-        telemetryManager.remove(sampling_index2);
     }
 }
